b877687467432101e1a0edb158ed94cd60387d7a
MarcosPotato <marquinhos2725@hotmail.com>
2022-07-14 20:22:29 -0300

    Finalizando desafio

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import { Genre } from '../@types/Films';
+
+import '../styles/sidebar.scss';
+
+interface SideNavProps {
+  selection: number
+  genres: Genre[]
+  onSelectGenre: (genre_id: number) => void 
+}
+
+export function SideBar({ genres, selection, onSelectGenre }: SideNavProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        { genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => onSelectGenre(genre.id)}
+              selected={ selection === genre.id }
+            />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
